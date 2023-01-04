75af224bd4cb17ae93083a8af2c968effea3844d
unknown <danilorkuwai@gmail.com>
2023-01-03 21:19:50 -0300

    aulas 15,16 e 17

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,9 +1,15 @@
+import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
-const tag = <strong>Olá React!</strong>
+import Primeiro from './components/basicos/Primeiro'
+
+
+
 const el = document.getElementById('root')
 ReactDOM.render(
-    <div>{tag}</div>,
+    <div>
+        <Primeiro></Primeiro>
+    </div>,
     el
-    )
+)

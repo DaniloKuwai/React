82f1a931325fe658b48031f2f6f9a45b6227d7ef
unknown <danilorkuwai@gmail.com>
2023-01-08 23:46:10 -0300

    aula 12 - Componente Card 01

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -4,17 +4,28 @@ import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from "./components/basicos/Aleatorio";
+import Card from "./components/layout/Card";
 
 export default function App() {
     return(
     <div id="app">
-        <h1>Fundamentos React 2</h1>
-        <Primeiro></Primeiro>
+        <h1>Fundamentos React</h1>
+        <Card titulo="#04 - Valor Aleatório">
+        <Aleatorio min={1} max={60}></Aleatorio>
+        </Card>
+
+        <Card titulo="#03 - Fragmento">
+        <Fragmento></Fragmento>
+        </Card>
+
+        <Card titulo="#02 - ComParametro">
         <ComParametro titulo="Situação do Aluno" aluno="Pedro" nota={9.3}></ComParametro>
         <ComParametro titulo="Situação do Aluno" aluno="Ana" nota={9.8}></ComParametro>
-        <Fragmento></Fragmento>
-        <h1>Desafio Número Aleatório</h1>
-        <Aleatorio min={1} max={60}></Aleatorio>
+        </Card>
+        
+        <Card titulo="#01 - Primeiro Componente">
+        <Primeiro></Primeiro>
+        </Card>
     </div>
     )
 }

d2ad250780f1a78068e73e602b57393bb9787a7e
unknown <danilorkuwai@gmail.com>
2023-01-09 23:24:44 -0300

    aula 14 e 15

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -6,6 +6,7 @@ import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from "./components/basicos/Aleatorio";
 import Card from "./components/layout/Card";
+import Familia from "./components/basicos/Familia"
 
 export default function App() {
     return(
@@ -13,20 +14,25 @@ export default function App() {
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
-        <Card titulo="#04 - Valor Aleatório">
+
+        <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
+        <Familia sobrenome="Ferreira"></Familia>
+        </Card>
+
+        <Card titulo="#04 - Valor Aleatório" color="#FA6900">
         <Aleatorio min={1} max={60}></Aleatorio>
         </Card>
 
-        <Card titulo="#03 - Fragmento">
+        <Card titulo="#03 - Fragmento" color="#E94C6F">
         <Fragmento></Fragmento>
         </Card>
 
-        <Card titulo="#02 - ComParametro">
+        <Card titulo="#02 - ComParametro" color="#E8B71A">
         <ComParametro titulo="Situação do Aluno" aluno="Pedro" nota={9.3}></ComParametro>
         <ComParametro titulo="Situação do Aluno" aluno="Ana" nota={9.8}></ComParametro>
         </Card>
         
-        <Card titulo="#01 - Primeiro Componente">
+        <Card titulo="#01 - Primeiro Componente" color="#588C73">
         <Primeiro></Primeiro>
         </Card>
         </div>

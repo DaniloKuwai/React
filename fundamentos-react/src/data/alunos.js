e87893c63a9d81da5ad990c2cd7e56d8ce31f3ce
unknown <danilorkuwai@gmail.com>
2023-01-10 15:17:09 -0300

    aula 17

---
+++ fundamentos-react/src/data/alunos.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    {id:1 , nome: 'Ana', nota:9.2},
+    {id:2 , nome: 'Bia', nota:8.4},
+    {id:3 , nome: 'Rafael', nota:6.8},
+    {id:4 , nome: 'Clara', nota:7.5},
+    {id:5 , nome: 'Igor', nota:4.6},
+    {id:6 , nome: 'José', nota:7.8},
+    {id:7 , nome: 'Marta', nota:3.7},
+    {id:8 , nome: 'Marcel', nota:6.9},
+    {id:9 , nome: 'Robson', nota:8},
+]
